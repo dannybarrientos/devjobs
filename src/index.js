b3d6d68c82495fce37ca879cc1c343156a5a80d2
Danny Barrientos <dannybarrientosagudelo@gmail.com>
2021-05-21 11:43:27 -0500

    creando el Servidor, El router y habilitando handlebars como template Engine

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const router = require('./router/index')
+const exphbs = require('express-handlebars')
+const path = require('path');
+
+const app = express();
+
+//TODO Habilitamos Handlebars como View
+app.engine('handlebars',
+    exphbs({
+        defaultLayout: 'layout'
+    })
+);
+app.set('view engine', 'handlebars');
+
+//TODO Static Files
+app.use(express.static(__dirname))
+
+app.use('/', router());
+
+app.listen(5000)

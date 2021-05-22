41be77c58ac447ff2d94bdbfb02e397fc675409d
Danny Barrientos <dannybarrientosagudelo@gmail.com>
2021-05-22 08:28:41 -0500

    DEfinicion del Modelo de Vacantes, Creacion de Middleware para validar los antes de validarÃon antes de validar

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
 const mongoose = require('mongoose');
 require('./config/db')
+
 const express = require('express')
 const exphbs = require('express-handlebars')
 const path = require('path');
 const router = require('../src/routes')
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
 
 require('dotenv').config({path :'variables.env'})
 
 const app = express();
 
 //TODO Habilitamos Handlebars como View
-app.engine('handlebars', 
+app.engine('handlebars',
     exphbs({
         defaultLayout: 'layout',
     })
@@ -19,6 +23,16 @@ app.set('view engine', 'handlebars');
 
 //TODO Static Files
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(cookieParser())
+//TODO Realizo el firmado de mi session
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    key: process.env.KEY,
+    resave: false,
+    saveUninitialized:false,
+    store: new MongoStore({ mongooseConnection: mongoose.connection })
+}));
 
 app.use('/', router());
 
---
+++ src/models/vacantes.js
@@ -0,0 +1,55 @@
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+const slug = require('slug');
+const shortid = require('shortid');
+
+const vacantesSchema = new mongoose.Schema({
+    titulo: {
+        type: String,
+        requerid: 'El nombre de la vancate es obligatorio',
+        trim: true
+    },
+    empresa :{
+        type: String,
+        trim: true
+    },
+    ubicacion: {
+        type: String,
+        trim: true,
+        required : 'La ubicacion es obligatoria'
+    },
+    salario: {
+        type: String,
+        default:0,
+        trim: true
+    },
+    contrato:{
+        type: String,
+        trim: true
+    },
+    descripcion: {
+        type: String,
+        trim: true
+    },
+    url: {
+        type: String,
+        lowercase: true
+    },
+    skill: [String],
+    candidatos:[{
+        nombre: String,
+        email: String,
+        cv: String
+    }]
+});
+
+//TODO Middleware para validar datos antes
+vacantesSchema.pre('save', function(next) {
+
+    //TODO Crear la URL con su unico id
+    const url = slug(this.titulo);
+    this.url =`${url}-${shortid.generate()}`;
+
+    next();
+})
+module.exports = mongoose.model('Vacante', vacantesSchema)

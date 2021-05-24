eb52fa14ae58b5908ecc83c1a906aa724b3d5976
Danny Barrientos <dannybarrientosagudelo@gmail.com>
2021-05-24 18:02:15 -0500

    Llenando los skill con los valores de la bases de datos

--- src/helpers/handlebars.js
+++ src/helpers/handlebars.js
@@ -1,6 +1,8 @@
 module.exports = {
     seleccionarSkills : (seleccionadas = [], opciones) => {
 
+        console.log(opciones.fn());
+
         const skills = ['HTML5', 'CSS3', 'CSSGrid', 'Flexbox', 'JavaScript', 'jQuery', 'Node', 'Angular', 'VueJS', 'ReactJS', 'React Hooks', 'Redux', 'Apollo', 'GraphQL', 'TypeScript', 'PHP', 'Laravel', 'Symfony', 'Python', 'Django', 'ORM', 'Sequelize', 'Mongoose', 'SQL', 'MVC', 'SASS', 'WordPress'];
 
         let html = '';
